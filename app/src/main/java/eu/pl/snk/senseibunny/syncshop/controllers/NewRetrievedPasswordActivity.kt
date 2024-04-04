c8cffd8012e6fe134ecb186ef07ceaab7d0573b5
SesneiBunny <lukasmatrix12345@gmail.com>
2024-04-04 19:21:07 +0200

    New password XML added

---
+++ app/src/main/java/eu/pl/snk/senseibunny/syncshop/controllers/NewRetrievedPasswordActivity.kt
@@ -0,0 +1,12 @@
+package eu.pl.snk.senseibunny.syncshop.controllers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import eu.pl.snk.senseibunny.syncshop.R
+
+class NewRetrievedPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_retrieved_password)
+    }
+}
