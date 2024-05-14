0ebf4835bcfe5d5dfededa5d3b173a17ca9dc519
SesneiBunny <lukasmatrix12345@gmail.com>
2024-05-14 18:14:10 +0200

    product info popup added and implemented

--- app/src/main/java/eu/pl/snk/senseibunny/syncshop/adapters/AddedProductAdapter.kt
+++ app/src/main/java/eu/pl/snk/senseibunny/syncshop/adapters/AddedProductAdapter.kt
@@ -1,11 +1,17 @@
 package eu.pl.snk.senseibunny.syncshop.adapters
 
+import android.R
 import android.app.Activity
 import android.app.AlertDialog
+import android.view.Gravity
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.PopupWindow
 import androidx.recyclerview.widget.RecyclerView
 import eu.pl.snk.senseibunny.syncshop.databinding.CellProductPlannedBinding
+import eu.pl.snk.senseibunny.syncshop.databinding.PopupProductInfoBinding
 import eu.pl.snk.senseibunny.syncshop.models.Model
 import eu.pl.snk.senseibunny.syncshop.models.Product
 import kotlinx.coroutines.Dispatchers
@@ -94,7 +100,39 @@ class AddedProductAdapter(private val productList: ArrayList<Product>, private v
                     .setNegativeButton("No", null)
                     .show()
             }
-        }
 
+            itemBinding.details.setOnClickListener{
+                showPopup(product)
+            }
+        }
+        fun showPopup(product: Product){
+            val popupBinding: PopupProductInfoBinding = PopupProductInfoBinding.inflate(LayoutInflater.from(itemBinding.root.context))
+            val popupView: View = popupBinding.root
+            val popupWindow = PopupWindow(popupView, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, true)
+
+            // Set the focusable property to true
+            popupWindow.isFocusable = true
+
+            // Set an OnTouchListener to consume touch events
+            popupView.setOnTouchListener { _, _ -> true }
+
+            // Set the text fields
+            popupBinding.pNameEt.text = product.nazwa
+            popupBinding.quantityEt.text = product.ilosc
+            popupBinding.shopEt.text = product.sklep
+            popupBinding.noteEt.text = product.notatka
+            popupBinding.createdEt.text = product.nazwaTworzacego
+            popupBinding.reservedEt.text = product.nazwaRezerwujacego
+            popupBinding.boughtEt.text = product.nazwaKupujacego
+
+            popupBinding.closeBtn.setOnClickListener {
+                popupWindow.dismiss()
+            }
+
+            // Show the popup window
+            popupWindow.showAtLocation(itemBinding.root, Gravity.CENTER, 0, 0)
+        }
     }
+
+
 }
